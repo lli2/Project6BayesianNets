ae5cdb0a1f523d9698c2ed687de7c2dff96e1663
djkiourtsis <djkiourtsis@wpi.edu>
2016-10-09 17:10:23 -0400

    Prepare network classes and functions
    This commit sets up the functions to be used in the Bayes Network
    classes.

---
+++ Project6BaysNets/src/BayesNet.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+
+public class BayesNet {
+    ArrayList<BayesNode> nodes;
+
+    public void createNet(String filename){
+        return;
+    }
+
+    public void cptTableMethodA(float[] vals){
+        return;
+    }
+
+    public void cptTableMethodB(float[] vals){
+        return;
+    }
+
+    public void assignStatus(char[] values){
+        return;
+    }
+
+    public void rejectionSampling(int numSamples){
+        return;
+    }
+
+    public void likelihoodWeighting(int numSamples){
+        return;
+    }
+}
--- Project6BaysNets/src/BayesNode.java
+++ Project6BaysNets/src/BayesNode.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+
 
 public class BayesNode {
+
+    String name;
+    ArrayList<BayesNode> parents;
+    int type;  // 0=query,  1=evidence,  2=unknown
+    float[][] cptTable;
+
+    public BayesNode(String name, ArrayList<BayesNode> parents, int type) {
+        super();
+        this.name = name;
+        this.parents = parents;
+        this.type = type;
+        this.cptTable = new float[2][parents.size()*2];
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public ArrayList<BayesNode> getParents() {
+        return parents;
+    }
+    public void addParent(BayesNode p) {
+        this.parents.add(p);
+    }
+    public int getType() {
+        return type;
+    }
+    public void setType(int type) {
+        this.type = type;
+    }
+    public float getCptValue(int r, int c) {
+        return cptTable[r][c];
+    }
+    public void modifyCptTable(int r, int c, float value) {
+        this.cptTable[r][c] = value;
+    }
 
 }
